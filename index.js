97c2bda023b7bc73595c1d81b5648a615b8be10e
Andrew Hansen <andrew89hansen@gmail.com>
2021-05-28 09:12:56 -0400

    wrong string for TZ.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [1, 5];
 rule.hour = 9;
 rule.minute = 30;
-rule.tz = "America/New York";
+rule.tz = "America/New_York";
 const job = schedule.scheduleJob(rule, () => {
     initFunc()
 });
